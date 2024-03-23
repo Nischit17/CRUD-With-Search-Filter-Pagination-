9fdc96e58dddbf536ea686f8a3c5dda1a89cfeba
Nischit17 <nischit819@gmail.com>
2024-03-23 17:31:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DataTabel from "./DataTabel";
+
+const App = () => {
+  return (
+    <div className="App">
+      <DataTabel />
+    </div>
+  );
+};
+
+export default App;
